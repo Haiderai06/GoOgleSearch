4f5f0c77b6d272c5aa4ee9151002dd6ded5a0890
Ali <Ali@DESKTOP-0QIDVL8>
2022-08-29 10:38:00 +0530

    This is demo project

---
+++ JenkinsPractice/src/test/java/com/google/page/GoogleSearchPage.java
@@ -0,0 +1,39 @@
+package com.google.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GoogleSearchPage {
+	 WebDriver driver;
+	String url = "https://www.google.com/";
+	
+  @BeforeTest
+  public void launchApplication	() {
+	  System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  System.out.println("Window is maximised.");
+	  driver.get(url);
+  }
+  
+  @Test
+  public void search() {
+	  WebElement searchInput = driver.findElement(By.xpath("//input[@title='Search']"));
+	  searchInput.sendKeys("Telus International");
+	  System.out.println("Input is entered.");
+	  WebElement searchBtn = driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']"));
+	  searchBtn.click();
+	  System.out.println("Search button is clicked.");
+  }
+  
+  @AfterTest
+  public void closeApplication() {
+	  driver.close();
+	  System.out.println("Application is closed.");
+  }
+}
